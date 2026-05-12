a7c9f9dc6fd46152ac5de6b9f9fa76cd38b891e0
fer4rie <firapuspita2106@gmail.com>
2026-05-12 17:47:56 +0700

    implementasi constructor dan destructor nya

--- jantung.h
+++ jantung.h
@@ -5,4 +5,12 @@ using namespace std;
 
 class jantung {
 public:
+    jantung() {
+        cout << "jantung dihidupkan\n";
+    }
+    ~jantung() {
+        cout << "jantung dimatikan\n";
+    }
+};
+#endif
 
